211c3aeaeb7c0e4d30baecaeab348fb63ea18adc
Robert Simpson <robert_simpson@hms.harvard.edu>
2017-04-26 15:36:06 -0400

    Add Tags Controller & Tag model, Tag get route.

--- app/Tag.php
+++ app/Tag.php
@@ -13,4 +13,10 @@ class Tag extends Model
 
     	return $this->belongsToMany(Post::class);
     }
+
+    public function getRouteKeyName()
+    {
+
+    	return 'name';
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,8 @@ Route::post('/posts', 'PostsController@store');
 
 Route::get('/posts/{post}', 'PostsController@show');
 
+Route::get('/posts/tags/{tag}', 'TagsController@index');
+
 
 Route::post('/posts/{post}/comments', 'CommentsController@store');
 
